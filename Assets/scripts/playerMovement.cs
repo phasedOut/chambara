590430f268b8e67cf4d5f8a5e9ecd7afdf8b5952
Yuji <yujisakai10@gmail.com>
2019-05-03 16:53:59 +0900

    first commit

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement: MonoBehaviour
+{
+    public void endAnimation()
+    {
+        isAnimating = false;
+    }
+
+    Animator anim;
+    Rigidbody rb;
+    private bool isAnimating = false;
+    float speed;
+    float lerpSpeed;
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    private bool keyHit = false;
+    private float fraction = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        speed = 10.0f;
+        lerpSpeed = 5f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            if (!isAnimating)
+            {
+                anim.SetBool("isRunning", true);
+                rb.velocity = transform.forward * speed;
+            }
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+            //rb.velocity = transform.forward * 0;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -30, 0) * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (!isAnimating) 
+            {
+                anim.SetTrigger("swing");
+                isAnimating = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (!isAnimating)
+            {
+                rb.velocity = transform.forward * 0;
+                isAnimating = true;
+                anim.SetTrigger("dodge");
+                startPos = transform.position;
+                endPos = transform.position + (transform.forward * -2f);
+                keyHit = true;
+            }
+        }
+        if (keyHit == true)
+        {
+            if (fraction < 1)
+            {
+                fraction += Time.deltaTime * lerpSpeed;
+                transform.position = Vector3.Lerp(startPos, endPos, fraction);
+            }
+            else if (fraction > 1)
+            {
+                keyHit = false;
+                fraction = 0;
+            }
+
+        }
+    }
+
+}
